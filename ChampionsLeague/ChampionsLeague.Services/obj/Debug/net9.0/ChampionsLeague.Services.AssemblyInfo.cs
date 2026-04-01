cf33be9090bd5a214848b7289c106a74f225747b
Lukas Devos <lukas.devos@student.vives.be>
2026-04-01 14:40:20 +0200

    debug commit

--- ChampionsLeague/ChampionsLeague.Services/obj/Debug/net9.0/ChampionsLeague.Services.AssemblyInfo.cs
+++ ChampionsLeague/ChampionsLeague.Services/obj/Debug/net9.0/ChampionsLeague.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChampionsLeague.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+485134a8db89241075a0a0f6a813fa0a5ac5b30f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46bf85809056a854a90d88fb893c9debb56da357")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChampionsLeague.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChampionsLeague.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
