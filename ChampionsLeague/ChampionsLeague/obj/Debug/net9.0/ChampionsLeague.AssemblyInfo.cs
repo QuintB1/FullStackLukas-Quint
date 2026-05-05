7c3574e5814a76c9b98291d944e16527ec5b1beb
QuintBroekaert <quint.broekaert@student.vives.be>
2026-05-05 10:49:20 +0200

    Home page decoration & api preperation

--- ChampionsLeague/ChampionsLeague/obj/Debug/net9.0/ChampionsLeague.AssemblyInfo.cs
+++ ChampionsLeague/ChampionsLeague/obj/Debug/net9.0/ChampionsLeague.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChampionsLeague")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+871f5c741ed9d7ca05a6cd27144b7946f8da6a64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae191c4f96399fd4db789b5c8c8e892d20bdaa2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChampionsLeague")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChampionsLeague")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
